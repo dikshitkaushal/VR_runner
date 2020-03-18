e0354336e10113aa2362568c6bf1c46d4023b2c6
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-03-18 14:26:21 +0530

    running and oscillating

---
+++ vr_runner/Assets/scripts/player_movement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float speed = 0f;
+    public float acceleration = 0.3f;
+    public float max_speed = 7f;
+    public float jumpingforce = 350f;
+    public bool isjumping=false;
+    private float timer = 0f;
+    player_oscillation oscillate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oscillate = GetComponentInChildren<player_oscillation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += acceleration * Time.deltaTime;
+        if (speed > max_speed)
+        {
+            speed = max_speed;
+        }
+        timer -= Time.deltaTime;
+        transform.position += speed * Vector3.forward * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) && isjumping==false)
+        {
+            jump();
+        }
+        if(timer<0)
+        {
+            isjumping = false;
+            oscillate.enabled = true;
+        }
+    }
+
+    public void jump()
+    {
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingforce);
+        isjumping = true;
+        oscillate.enabled = false;
+        timer = 1.5f;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+    }
+}
---
+++ vr_runner/Assets/scripts/player_oscillation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_oscillation : MonoBehaviour
+{
+    public float amplitude = 2f;
+    public float frequency = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oscillate();
+    }
+
+    public void oscillate()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Cos(transform.position.z * frequency) * amplitude, transform.localPosition.z);
+    }
+}
