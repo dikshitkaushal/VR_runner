6aa4502fa054250b320d2e2d6f3c4bc9b9f81623
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-03-21 11:55:12 +0530

    sound and visual effects

--- vr_runner/Assets/scripts/gamecontroller.cs
+++ vr_runner/Assets/scripts/gamecontroller.cs
@@ -6,15 +6,48 @@ using UnityEngine.SceneManagement;
 public class gamecontroller : MonoBehaviour
 {
     public player_movement player;
+    public AudioClip[] clips;
+    private AudioSource source;
+    AudioSource crowdvoice;
     public TextMesh infotext;
     private float gametimer = 0f;
     private float restarttimer = 3f;
+    private void Awake()
+    {
+        player.enabled = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        crowdvoice = GameObject.Find("Player").GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>();
+        start_the_race();
     }
 
+    public void start_the_race()
+    {
+        crowdvoice.volume = 0.28f;
+        playaudio(0);
+        StartCoroutine(set());
+    }
+    IEnumerator set()
+    {
+        yield return new WaitForSeconds(1f);
+        playaudio(1);
+        StartCoroutine(gunshot());
+    }
+    IEnumerator gunshot()
+    {
+        yield return new WaitForSeconds(1.8f);
+        playaudio(2);
+        crowdvoice.volume = 0.6f;
+        player.enabled = true;
+    }
+    public void playaudio(int clip)
+    {
+        source.clip = clips[clip];
+        source.Play();
+    }
     // Update is called once per frame
     void Update()
     {
